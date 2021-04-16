b72fb62d5301fdd052ba51dc7bf3514ec0eeaebb
Gaurav Sen <gaurav.s3n@gmail.com>
2021-04-16 22:20:01 +0530

    removed port

--- public/script.js
+++ public/script.js
@@ -2,8 +2,7 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 
 const myPeer = new Peer(undefined, {
-  host: "https://videoconference-peer.onrender.com/",
-  port: "3001",
+  host: "https://videoconference-peer.onrender.com/"
 });
 let index = 0;
 const myVideo = document.createElement("video");
